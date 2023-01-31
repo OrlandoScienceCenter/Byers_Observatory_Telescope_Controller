61a20955b36df74936a013483a9b3f118ea4719b
DSikes <david@sikespc.com>
2023-01-31 08:31:03 -0500

    Testing using arduino as clock

--- Code/Arduino/originalPaddleTest/originalPaddleTest.ino
+++ Code/Arduino/originalPaddleTest/originalPaddleTest.ino
@@ -2,10 +2,14 @@ void setup()
 {
   Serial.begin(4800);
 
+  pinMode(3, OUTPUT);
+
   Serial.println(F("Init done..."));
+
+  Serial.println(F("PWM START NOW"));
 }
 
 void loop() 
 {
-  
+   analogWrite(3, 512);
 }
